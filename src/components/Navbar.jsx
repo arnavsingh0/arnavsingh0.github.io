604a5a944536f60d79b4263dc4253b0210dbd8bd
Arnav Singh <165240307+arnavsingh0@users.noreply.github.com>
2024-10-02 01:29:37 -0400

    A New Hope

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,18 +2,18 @@ import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <header className='header'>
+    <header className='header flex justify-between items-center'>
       <NavLink
         to='/'
         className={({ isActive }) =>
           isActive ? 'text-starry-gold' : 'text-deep-space-blue hover:text-galaxy-purple'
         }
       >
-        <h1 className='text-4xl font-bold transition duration-300 ease-in-out'>
+        <h1 className='text-4xl font-bold transition duration-300 ml--2 ease-in-out pulsating-as'> {/* Add the pulsating class */}
           AS
         </h1>
       </NavLink>
-      <nav className='flex text-lg gap-10 font-medium'>
+      <nav className='flex ml-7 mt-7  text-lg gap-10 font-medium'>
         {["about", "projects", "education", "skills", "research", "experience", "academic-projects", "contact"].map((link, index) => (
           <NavLink
             key={index}
@@ -21,7 +21,7 @@ const Navbar = () => {
             className={({ isActive }) =>
               isActive
                 ? "text-blue-600"
-                : "text-black hover:text-blue-500 transition duration-300 ease-in-out"
+                : "text-white hover:text-blue-500 transition duration-300 ease-in-out"
             }
           >
             {link.charAt(0).toUpperCase() + link.slice(1).replace(/-/g, ' ')}
@@ -31,5 +31,4 @@ const Navbar = () => {
     </header>
   );
 };
-
 export default Navbar;
